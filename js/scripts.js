195f758e91a6a44c1e41d2d4d4dee4726d158cef
rodneyspark <gakururodney@gmail.com>
2020-10-12 12:13:16 +0300

    inital commit

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#dev-img").click(function(){
+    $("#dev-img").toggle();
+    $("#development").toggle();
+  });
+      $("#development").click(function(){
+        $("#development").slideUp();
+        $("#dev-img").slideDown();
+      });
+     
+  });
+  $(document).ready(function(){
+    $("#design-img").click(function(){
+    $("#design-img").toggle();
+    $("#design").toggle();
+  });
+      $("#design").click(function(){
+        $("#design").slideUp();
+        $("#design-img").slideDown();
+      });
+     
+  });
+  $(document).ready(function(){
+    $("#product-img").click(function(){
+    $("#product-img").toggle();
+    $("#product").toggle();
+  });
+      $("#product").click(function(){
+        $("#product").slideUp();
+        $("#product-img").slideDown();
+      });
+  });
+  $(document).ready(function(){
+    $('#contact form').submit(function (){
+        let name  =  $('input#name').val();
+        let email  = $('input#email').val()
+        let  message = $('input#message').val()
+
+        alert('Hello ' + name + ' we have received your message thank you for reaching out')
+    });
+})
